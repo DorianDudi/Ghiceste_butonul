cff9165a0890a1725966402d5499d31d99e511aa
Dorian-DV <ddorianvlad@yahoo.ro>
2022-08-28 22:40:05 +0300

    Create theScripts.js

---
+++ theScripts.js
@@ -0,0 +1,16 @@
+function getRandomIntFromTo(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let winning_number = getRandomIntFromTo(1, 3);
+document.getElementById('winner').innerText = winning_number;
+
+function getClickID() {
+	if(event.target.id == winning_number) {
+  	alert("Correct!");
+    winning_number = getRandomIntFromTo(1, 3);
+    document.getElementById('winner').innerText = winning_number;
+  } else {
+  	alert("Wrong!");
+  }
+}
