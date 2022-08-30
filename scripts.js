d54c1ed4b62ff4201af0f14289bd8922b48c88f6
Dorian-DV <ddorianvlad@yahoo.ro>
2022-08-30 17:38:46 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function getRandomIntFromTo(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 let winning_number = getRandomIntFromTo(1, 3);
@@ -7,10 +7,10 @@ document.getElementById('winner').innerText = winning_number;
 
 function getClickID() {
 	if(event.target.id == winning_number) {
-  	alert("Correct!");
-    winning_number = getRandomIntFromTo(1, 3);
-    document.getElementById('winner').innerText = winning_number;
-  } else {
-  	alert("Try, try... Try again!");
-  }
+		alert("Correct!");
+		winning_number = getRandomIntFromTo(1, 3);
+		document.getElementById('winner').innerText = winning_number;
+	} else {
+		alert("Try, try... Try again!");
+	}
 }
